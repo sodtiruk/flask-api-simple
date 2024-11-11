0e5cca1105ef1add5d13dc7c42651f9b24aa20bd
Sutthirak Sutsaenya <sodtiruk22@gmail.com>
2024-11-11 23:16:58 +0700

    build: update dockerfile migrate database and delete routes test

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,14 @@ WORKDIR /src
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
+#lib for crytography in python
+RUN apk add --no-cache gcc musl-dev libffi-dev openssl-dev
+
 COPY . .
 
 EXPOSE 5000
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+# CMD [ "flask", "db", "upgrade", "&&", "python3", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD flask db upgrade && python3 -m flask run --host=0.0.0.0
+# CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+
--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,31 @@
 from flask import Flask
-
-from .routes import main
-
 from app.controller.UserController import user
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app():
     #create app    
     app = Flask(__name__)
 
-    app.register_blueprint(main)
+    #config migrate
+    #localhost
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3307/simple'
+    #docker
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@db:3306/simple'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(128), nullable=False)
+        email = db.Column(db.String(128), unique=True, nullable=False)
+        def __repr__(self):
+            return f"<User {self.name}>"
+
+    #config routes
     app.register_blueprint(user, url_prefix='/api')
 
 
